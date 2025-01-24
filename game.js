8fca2aa9e9ebf28a164229a563fc2ff61008fd14
1vanSher <ivan.sher.05@mail.ru>
2025-01-24 03:15:16 +0300

    Создана функция бинарного поиска

---
+++ game.js
@@ -0,0 +1,32 @@
+function getRandomNumer(max){
+    return Math.floor(Math.random() * max);
+}
+
+const secretNumber = getRandomNumer(100);
+console.log('Компьютер загадал число ' + secretNumber);
+
+function searchNumber(){
+    let max = 100;
+    let min  = 1;
+
+    while(min < max){
+
+        let currentNumber = Math.floor((min + max)/2);
+
+        console.log(`Компьютер 2: Пробую число ${currentNumber}.`);
+
+        if(currentNumber == secretNumber){
+            console.log(`Компьютер 1: Угадал! `);
+
+            break;
+        }else if(currentNumber < secretNumber){
+            console.log(`Компьютер 1: Больше.`);
+            min = currentNumber;
+        }else if(currentNumber > secretNumber){
+            console.log(`Компьютер 1: Меньше.`);
+            max = currentNumber;
+
+        }
+    } 
+}
+searchNumber();
