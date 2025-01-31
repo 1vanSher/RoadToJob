97d3cb9270fa7342e8bd73fb20a7929e0abc0801
1vanSher <ivan.sher.05@mail.ru>
2025-01-31 19:50:44 +0300

    разработана функция, сокращающая строку по ограничению символов

--- string.js
+++ string.js
@@ -1,8 +1,22 @@
 function capitalLetter(str){
     firstLetter = str.at(0);
+    
     if(firstLetter === firstLetter.toLowerCase()){
        return firstLetter.toUpperCase() + str.slice(1);
     }
     return str;
 }
-console.log(capitalLetter('пока'));
+
+
+function paragraphBreak(text, maxLength) {
+    if(text.length <= maxLength) return text;
+
+    let truncated = text.slice(0, maxLength);
+
+    let lastSpace = truncated.lastIndexOf(" ");
+    let lastPunctuation = truncated.search(/[,\.!?:;][^,\.!?:;]*$/);
+    let cutIndex = Math.max(lastSpace, lastPunctuation);
+
+    truncated = truncated.slice(0, cutIndex); 
+    return truncated + "..."; 
+}
